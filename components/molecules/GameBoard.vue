5b6d4260df68e5dc5901b4d5c959cc49ca1de046
dorarep <ishikami.naotaka@gmail.com>
2018-11-03 04:16:29 +0900

    delete unuse codes.

--- components/molecules/GameBoard.vue
+++ components/molecules/GameBoard.vue
@@ -2,8 +2,8 @@
   transition-group(name="list-complete" tag="div")
     .tr(v-for="(row, y) in $store.state.game.board" :key="y")
       cell.td(v-for="(cell, x) in row" @click.native="$store.dispatch('game/onClick', {x, y})" :key="x")
-        player(key="p" v-if="$store.state.game.player.x === x && $store.state.game.player.y === y")
-        enemy(key="e" v-if="$store.state.game.enemy.x === x && $store.state.game.enemy.y === y")
+        player(v-if="$store.state.game.player.x === x && $store.state.game.player.y === y")
+        enemy(v-if="$store.state.game.enemy.x === x && $store.state.game.enemy.y === y")
         wall(v-if="cell == 1")
 </template>
 
@@ -30,19 +30,5 @@
 
   .td {
     display: table-cell;
-  }
-
-  .list-complete-item {
-    transition: all 1s;
-    display: inline-block;
-    margin-right: 10px;
-  }
-  .list-complete-enter, .list-complete-leave-to
-    /* .list-complete-leave-active for below version 2.1.8 */ {
-    opacity: 0;
-    transform: translateY(30px);
-  }
-  .list-complete-leave-active {
-    position: absolute;
   }
 </style>
